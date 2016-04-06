e791a8c77025b03c4580f5b9a7aea03a3854d0a6
Ajay Nath MS <msajaynath@gmail.com>
2016-04-06 23:10:04 +0530

    Final reviews commit.

--- app/src/main/java/pms/com/patientmonitor/DoctorHomeActivity.java
+++ app/src/main/java/pms/com/patientmonitor/DoctorHomeActivity.java
@@ -6,11 +6,13 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class DoctorHomeActivity extends AppCompatActivity {
-    ImageView myinfo,patlist,logout;
+    ImageView myinfo,patlist,logout,search;
+    EditText searchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,30 @@ public class DoctorHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_doctor_home);
         myinfo= (ImageView) findViewById(R.id.my_bio);
         patlist= (ImageView) findViewById(R.id.my_report);
+        searchText= (EditText) findViewById(R.id.patSearch);
         logout= (ImageView) findViewById(R.id.logout);
+        search= (ImageView) findViewById(R.id.search);
 
         patlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(DoctorHomeActivity.this,MyPatientsListActivity.class));
+
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(searchText.getText().toString().length()==0)
+                {
+                    Toast.makeText(getApplicationContext(),"Please enter some text!!!",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                   Intent in= new Intent(DoctorHomeActivity.this, MyPatientsListActivity.class);
+                    in.putExtra("search",searchText.getText().toString());
+                    startActivity(in);
+                }
 
             }
         });
--- app/src/main/java/pms/com/patientmonitor/MyPatientsListActivity.java
+++ app/src/main/java/pms/com/patientmonitor/MyPatientsListActivity.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import Entity.Patient;
@@ -17,34 +19,84 @@ import Entity.Patient;
 public class MyPatientsListActivity extends ListActivity {
 
     private List<Patient> ptlist;
+    private TextView headText;
+    private List<Patient> temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_patients_list);
+        headText= (TextView) findViewById(R.id.headText);
         final SharedPreferences sharedpreferences = getSharedPreferences("PMS", Context.MODE_PRIVATE);
         long doctorid=sharedpreferences.getLong("userid", 0);
 
-        ptlist=Patient.findWithQuery(Patient.class, "Select * from Patient where doctor = ?",doctorid+"");
-        String[] patNames=new String[ptlist.size()];
-        for(int i=0;i<ptlist.size();i++)
+        Intent in =getIntent();
+        if(!in.hasExtra("search"))
         {
-            patNames[i]=ptlist.get(i).patient.name.toString();
+            ptlist=Patient.findWithQuery(Patient.class, "Select * from Patient where doctor = ?", doctorid + "");                 temp=ptlist;
+            temp=ptlist;
+            String[] patNames=new String[ptlist.size()];
+            for(int i=0;i<ptlist.size();i++)
+            {
+                patNames[i]=ptlist.get(i).patient.name.toString();
+
+            }
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_list_item_1, patNames);
+
+
+            // Assign adapter to List
+            setListAdapter(adapter);
+        }
+
+        else
+        {
+            String searchText=in.getStringExtra("search");
+            ptlist=Patient.findWithQuery(Patient.class, "Select * from Patient where doctor = ?", doctorid + "");
+
+                List<String> patNames = new ArrayList<String>();
+                 temp=ptlist;
+              //  String[] patNames=new String[ptlist.size()];
+                for(int i=0;i<ptlist.size();i++)
+                {
+                    String texts=ptlist.get(i).patient.name.toString().toLowerCase();
+
+                    if(texts.contains(searchText.toLowerCase()))
+                    patNames.add(ptlist.get(i).patient.name.toString());
+                    else
+                    {
+                        temp.remove(i);
+                    }
+
+                }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1, patNames);
+
+
+                // Assign adapter to List
+                if(patNames.size()==0)
+                {
+                    headText.setText("No patients found!!!");
+                }
+            else {
+
+
+                    setListAdapter(adapter);
+                }
 
         }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, patNames);
 
 
-        // Assign adapter to List
-        setListAdapter(adapter);
+
+
+
 
     }
 
     @Override
     protected void onListItemClick (ListView l, View v, int position, long id) {
         Intent intent=new Intent(MyPatientsListActivity.this,AddReportActivity.class);
-        intent.putExtra("patid",ptlist.get(position).patient.getId());
+        intent.putExtra("patid",temp.get(position).patient.getId());
         startActivity(intent);
     }
 }
